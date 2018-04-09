dd7dbe6a072768400faec931c8e2cab871800b12
andreksrynt <andrekisriyanto>
2018-04-09 08:48:21 +0700

    FIX LAYOUT REVIEW

--- app/src/main/java/skeletonteam/traveguide/Review.java
+++ app/src/main/java/skeletonteam/traveguide/Review.java
@@ -1,7 +1,10 @@
 package skeletonteam.traveguide;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.view.View;
 
 public class Review extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class Review extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_review);
+
+        ImageButton backButton = (ImageButton) findViewById(R.id.arrowback);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Tour.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/skeletonteam/traveguide/Tour.java
+++ app/src/main/java/skeletonteam/traveguide/Tour.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 public class Tour extends AppCompatActivity {
@@ -14,10 +15,19 @@ public class Tour extends AppCompatActivity {
         setContentView(R.layout.activity_tour);
 
         ImageButton backButton = (ImageButton) findViewById(R.id.arrowback);
+        Button reviewButton = (Button) findViewById(R.id.btn_review);
+
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), City.class);
+                startActivity(intent);
+            }
+        });
+        reviewButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Review.class);
                 startActivity(intent);
             }
         });
